15f774bfe80db75adac3e8651d6f83e62a0f2729
Gunjan Datta <me@dattabase.com>
2017-01-21 17:48:52 -0500

    Added the project. Updated the JSLink for the "View".

---
+++ project/GD.SPREST.JSLink/Features/Feature1/Feature1.feature
@@ -0,0 +1,12 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="07e546c4-b9f8-4552-b879-67e420a84882" description="SharePoint Add-in Feature" featureId="07e546c4-b9f8-4552-b879-67e420a84882" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="GD.SPREST.JSLink Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="f60ce23b-4ab6-493a-a2a3-0ddd7b24f06f" />
+    <projectItemReference itemId="05544efe-077a-4788-b63e-edd5ae4cc46e" />
+    <projectItemReference itemId="a05683e9-b87b-434e-be3d-2cf3ad20fe87" />
+    <projectItemReference itemId="0b2687e0-df00-44d0-b312-96740140a56f" />
+    <projectItemReference itemId="75ab1f02-d163-4930-a093-04c7b3136df8" />
+    <projectItemReference itemId="4bd537fc-c447-4587-8600-39e12dd57c99" />
+    <projectItemReference itemId="ada17027-e6bc-45de-a539-2195529148b4" />
+  </projectItems>
+</feature>
---
+++ project/GD.SPREST.JSLink/Scripts/view.js
@@ -0,0 +1,105 @@
+﻿"use strict";
+
+/**
+ * JSLink: View Customization
+ */
+var GDView = (function () {
+    /**
+     * Methods
+     */
+
+    // Method to add tags above and below the body
+    function body(ctx) {
+        // Render the default html
+        var html =
+            "<tbody><tr><td colspan='99'><h2 style='background-color:orange; color:white;'>Above Body</h2></td></tr></tbody>" +
+            (RenderBodyTemplate ? RenderBodyTemplate(ctx) : "") +
+            "<tbody><tr><td colspan='99'><h2 style='background-color:orange; color:white;'>Below Body</h2></td></tr></tbody>";
+
+        // Return the html
+        return html;
+    }
+
+    // Method to add tags above and below the footer
+    function footer(ctx) {
+        // Render the default html
+        var html = "<h1 style='background-color:blue; color:white;'>Above Footer</h1>" +
+            RenderFooterTemplate(ctx) +
+            "<h1 style='background-color:blue; color:white;'>Below Footer</h1>";
+
+        // Return the html
+        return html;
+    }
+
+    // Method to add tags above and below the group
+    function group(ctx) {
+        var group = document.createElement("table");
+
+        // Render the default html
+        group.innerHTML = RenderGroupTemplate(ctx);
+
+        // Get the row
+        var row = group.querySelector("tr");
+
+        // Add the tags
+        row.innerHTML =
+            "<td><span style='background-color:purple; color:white;'>Before Group</span></td>" +
+            row.innerHTML +
+            "<td><span style='background-color:purple; color:white;'>After Group</span></td>";
+
+        // Return the html
+        return group.innerHTML;
+    }
+
+    // Method to add tags above and below the header
+    function header(ctx) {
+        // Render the default html
+        var html =
+            "<h1 style='background-color:brown; color:white;'>Above Header</h1>" +
+            RenderHeaderTemplate(ctx) +
+            "<h1 style='background-color:brown; color:white;'>Below Header</h1>";
+
+        // Return the html
+        return html;
+    }
+
+    // Method to add tags above and below the item
+    function item(ctx) {
+        // Render the default html
+        var html =
+            "<tr><td><h2 style='background-color:green; color:white;'>Above Item</h2></td></tr>" +
+            RenderItemTemplate(ctx) +
+            "<tr><td><h2 style='background-color:green; color:white;'>Below Item</h2></td></tr>";
+
+        // Return the html
+        return html;
+    }
+
+    /**
+     * Initialization
+     */
+    function init() {
+        var jsLink = new $REST.JSLink();
+        jsLink.Templates = {
+            BaseViewId: 2,
+            Body: body,
+            Footer: footer,
+            Group: group,
+            Header: header,
+            Item: item
+        };
+
+        // Apply the customization
+        SPClientTemplates.TemplateManager.RegisterTemplateOverrides(jsLink.getTemplate());
+    }
+
+    /**
+     * Public Interface
+     */
+    return {
+        init: init
+    };
+})();
+
+// Write the javascript to initialize the CSR override. This will ensure it's called when MDS is enabled
+document.write("<script type='text/javascript'>(function() { GDView.init(); })();</script>");
